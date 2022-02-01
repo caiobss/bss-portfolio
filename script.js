d99283fda095651b2d3269831fdb95af41359f1f
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-02-01 19:17:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburguer = document.querySelector(".hamburguer")
+const menua = document.querySelector('.menu')
 hamburguer.addEventListener('click', clicar)
+menua.addEventListener('click', clicar)
 function clicar(){
     const container = document.querySelector('.container')
     container.classList.toggle('mostre-side-bar')

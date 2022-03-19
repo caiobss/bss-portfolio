b6c3a1b6eed3d4bcabb4b9f86a0bde610277446a
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-03-19 18:11:24 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const bntarteinfo = document.querySelector('.bnt-arte-info')
-bntarteinfo.addEventListener('click', clicar)
-function clicar(){
-    const allpage = document.querySelector('.allpage')
-    allpage.classList.toggle("mostre-down-bar")
-}

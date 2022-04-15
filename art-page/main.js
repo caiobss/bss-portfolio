ab426c3e16fef137d23522bdfffa337802213da4
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-04-14 23:04:00 -0300

    Add files via upload

--- art-page/main.js
+++ art-page/main.js
@@ -1,6 +1,6 @@
 const bntarteinfo = document.querySelector('.bnt-arte-info')
 bntarteinfo.addEventListener('click', clicar)
 function clicar(){
-    const allpage = document.querySelector('.allpage')
+    const allpage = document.querySelector('.body-art')
     allpage.classList.toggle("mostre-down-bar")
 }
